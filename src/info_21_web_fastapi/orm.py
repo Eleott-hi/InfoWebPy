66c6dd15f38eb76d121b9efe2c6f79690a2c123f
Eleott-hi <laptev18vv@gmail.com>
2024-01-03 00:45:54 +0700

    add postgres docker + learning fast api/sqlalchimy/react

--- src/info_21_web_fastapi/orm.py
+++ src/info_21_web_fastapi/orm.py
@@ -4,6 +4,9 @@ engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
 
 with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+    )
     conn.commit()
