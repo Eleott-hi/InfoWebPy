9ad51cbeb1620d7b00c0ae5586439293d4b08005
Eleott-hi <laptev18vv@gmail.com>
2024-02-15 01:15:21 +0700

    logging

--- src/fastapi/logging_config.py
+++ src/fastapi/logging_config.py
@@ -5,6 +5,39 @@ import os
 from datetime import datetime
 
 
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "handlers": {
+#         "file": {
+#             "level": "INFO",
+#             "class": "logging.handlers.TimedRotatingFileHandler",
+#             "filename": BASE_DIR.joinpath(
+#                 "logs",
+#                 f'logs_{datetime.now().strftime("%d-%m-%y-%H-%M-%S")}.log',
+#             ),
+#             "when": "D",
+#             "interval": 1,
+#             "formatter": "verbose",
+#             "delay": False,
+#         },
+#     },
+#     "loggers": {
+#         "retail_app": {
+#             "handlers": ["file"],
+#             "level": "INFO",
+#             "propagate": True,
+#         }
+#     },
+#     "formatters": {
+#         "verbose": {
+#             "format": "{levelname} UTC {asctime} {message}",
+#             "style": "{",
+#         },
+#     },
+# }
+
+
 def configure_logging_from_json(config_file):
     with open(config_file, "r") as f:
         config = json.load(f)
